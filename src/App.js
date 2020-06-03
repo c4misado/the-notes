47ea55a7564ee47934289d52bf469c146bc9c968
c4misado <adelmanalec@gmail.com>
2020-06-03 17:19:53 +0300

    added some things, need to fix thing with %

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import String from "./components/String"
+import String from "./components/String";
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+  render(){
+  return ( 
     <div className="App">
-      <String string="LowE"/>
-      <String string="A"/>
-      <String string="D"/>
-      <String string="G"/>
-      <String string="B"/>
-      <String string="HighE"/>
+      <String string="LowE" openNote={-7}/>
+      <String string="A" openNote={0}/>
+      <String string="D" openNote={5}/>
+      <String string="G" openNote={10}/>
+      <String string="B" openNote={14}/>
+      <String string=" HighE" openNote={19}/>
     </div>
-  );
+    )
+  }
 }
+
 
 export default App;
--- src/components/String.js
+++ src/components/String.js
@@ -13,28 +13,31 @@ class String extends React.Component {
         return (
             <div className="String">
                 <ul id={this.props.string}>
-                    <li><Note symbol="Open" value={this.state.openNote}/></li>
-                    <li><Note symbol="-" value={this.state.openNote + 1}/></li>
-                    <li><Note symbol="-"/></li>
-                    <li> <Note symbol="•"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li> <Note symbol="•"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li> <Note symbol="•"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li> <Note symbol="•"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li> <Note symbol="••"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li> <Note symbol="•"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li> <Note symbol="•"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li> <Note symbol="•"/></li>
-                    <li><Note symbol="-"/></li>
-                    <li> <Note symbol="•"/></li>
+
+
+            //Fix this section with the % thingy
+                    {/* <li><Note symbol="Open" value={this.props.openNote % 12 < 0 ? this.props.openNote % 12 + 12 : this.props.openNote + 1 % 12}/></li>
+                    <li><Note symbol="-" value={this.props.openNote + 2% 12 < 0 ? this.props.openNote % 12 + 12 : this.props.openNote + 3 % 12}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 4 % 12 < 0 ? this.props.openNote % 12 + 12 : this.props.openNote + 5 % 12}/></li> */}
+
+                    <li><Note symbol="-"value={this.props.openNote + 4}/></li>
+                    <li> <Note symbol="•"value={this.props.openNote + 5}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 6}/></li>
+                    <li> <Note symbol="•"value={this.props.openNote + 7}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 8}/></li>
+                    <li> <Note symbol="•"value={this.props.openNote + 9}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 10}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 11}/></li>
+                    <li> <Note symbol="••"value={this.props.openNote + 12 % 12}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 13 % 12}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 14 % 12}/></li>
+                    <li> <Note symbol="•"value={this.props.openNote + 15 % 12}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 16 % 12}/></li>
+                    <li> <Note symbol="•" value={this.props.openNote + 17 % 12}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 18 % 12}/></li>
+                    <li> <Note symbol="•"value={this.props.openNote + 19 % 12}/></li>
+                    <li><Note symbol="-"value={this.props.openNote + 20 % 12}/></li>
+                    <li> <Note symbol="•"value={this.props.openNote + 21 % 12}/></li>
                 </ul>
             </div>
         )
