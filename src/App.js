aa6fd6a2e3ab8b92fe97dfb9d551ff6c0d34b393
c4misado <adelmanalec@gmail.com>
2020-06-10 13:52:07 +0300

    added tonic note of key selector

--- src/App.js
+++ src/App.js
@@ -7,17 +7,41 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      key: 0,
+      scale: []
     }
   }
+
+  changeKey = (event) => {
+    this.setState({key: event.target.value})
+  }
+
+
   render(){
   return ( 
     <div className="App">
-      <String openNote={115}/>
+      <h1>{this.state.key}</h1>
+      <label for="Select Key">Select a key:</label>
+      <select name="Select Key" onChange={this.changeKey}>
+        <option value="0">A</option>
+        <option value="1">A sharp</option>
+        <option value="2">B</option>
+        <option value="3">C</option>
+        <option value="4">C sharp</option>
+        <option value="5">D</option>
+        <option value="6">D sharp</option>
+        <option value="7">E</option>
+        <option value="8">F</option>
+        <option value="9">F sharp</option>
+        <option value="10">G</option>
+        <option value="11">G sharp</option>
+      </select>
+      {/* <String openNote={115}/>
       <String openNote={120}/>
       <String openNote={125}/>
       <String openNote={130}/>
       <String openNote={134}/>
-      <String openNote={139}/>
+      <String openNote={139}/> */}
     </div>
     )
   }
